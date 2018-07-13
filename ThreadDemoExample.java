7686fdc7402ea86eeb236448899eac674573ce92
babul3210 <babul.cu.cse35@gmail.com>
2018-07-13 11:18:58 +0600

    Java Thread 20180713

---
+++ ThreadDemoExample.java
@@ -0,0 +1,6 @@
+public class ThreadDemoExample{
+	public static void main(String[]args){
+		Thread currentThread  = Thread.currentThread();
+		System.out.println("CurrentThread name: "+currentThread.getName());
+		}
+	}
